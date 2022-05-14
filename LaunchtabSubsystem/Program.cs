db4156f2b867d17c4b1e31533d0be07ecbfe87e8
Naquino14 <Naquino14@outlook.com>
2022-05-14 17:45:11 -0400

    Functioning power button!

--- LaunchtabSubsystem/Program.cs
+++ LaunchtabSubsystem/Program.cs
@@ -2,32 +2,47 @@
 using c = System.Console;
 using System.Device.Gpio;
 using System.Diagnostics;
+using System.Threading;
 
-c.WriteLine("Subsustem is now running. Press any key to exit.");
-int pin = 3;
+c.WriteLine("Subsystem is now running. Press ctrl+c to exit.");
+int powerButtonPin = 3;
+bool pressedState = false;
 using var controller = new GpioController();
-controller.OpenPin(pin, PinMode.Input);
+controller.OpenPin(powerButtonPin, PinMode.Input);
 
 //pin update callbacks
-controller.RegisterCallbackForPinValueChangedEvent(pin, PinEventTypes.Falling, (pin, value) =>
-{
-    c.WriteLine($"Falling! Pin {pin} changed to {value}");
-});
+controller.RegisterCallbackForPinValueChangedEvent(powerButtonPin, PinEventTypes.Falling, (pin, value) => { pressedState = false; });
 
-controller.RegisterCallbackForPinValueChangedEvent(pin, PinEventTypes.Rising, (pin, value) =>
-{
-    c.WriteLine($"Rising! Pin {pin} changed to {value}");
-});
+controller.RegisterCallbackForPinValueChangedEvent(powerButtonPin, PinEventTypes.Rising, (pin, value) => { pressedState = true; });
 
 // exit warning
 AppDomain.CurrentDomain.ProcessExit += (sender, args) =>
 {
-    c.WriteLine("Warning: Subsystem is shutting down. Press any key to continue, or 'r' to restart.");
+    c.WriteLine("\rWarning: Subsystem is shutting down. Press any key to continue, or 'r' to restart.");
     var key = c.ReadKey();
     if (key.KeyChar == 'r') // restart subsystem
         try { Process.Start(Process.GetCurrentProcess().MainModule!.FileName!); }
         catch (Exception) { c.WriteLine("Error restarting subsystem."); }
-    //controller.ClosePin(pin);
+    c.Write('\r');
 };
 
-c.ReadKey();
+Action Shutdown = () =>
+{
+    c.WriteLine("\rShutdown request recieved.");
+    new Process(){ StartInfo = new("sudo", "shutdown -h now") }.Start();
+};
+
+int gestLoopCounter = 0;
+for (;;)
+{
+    if (pressedState)
+    {
+        if (gestLoopCounter > 6) // aprox 1.25 sec
+            Shutdown();
+        gestLoopCounter++;
+    }
+    else
+        gestLoopCounter = 0;
+
+    Thread.Sleep(200);
+}
