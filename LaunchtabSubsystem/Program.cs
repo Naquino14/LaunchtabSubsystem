716af1aa867c14453d2999dc86a9283811930f13
Nathaniel Aquino <37340195+Naquino14@users.noreply.github.com>
2022-05-14 16:06:59 -0400

    exit warning tests

--- LaunchtabSubsystem/Program.cs
+++ LaunchtabSubsystem/Program.cs
@@ -1,9 +1,33 @@
 ﻿using System;
 using c = System.Console;
 using System.Device.Gpio;
-using System.Threading;
+using System.Diagnostics;
 
 c.WriteLine("Subsustem is now running. Press any key to exit.");
 int pin = 18;
-using var controller = new GpioController();
-controller.OpenPin(pin, PinMode.Output);
+//using var controller = new GpioController();
+//controller.OpenPin(pin, PinMode.Input);
+
+// pin update callbacks
+//controller.RegisterCallbackForPinValueChangedEvent(pin, PinEventTypes.Falling, (pin, value) =>
+//{
+//    c.WriteLine($"Falling! Pin {pin} changed to {value}");
+//});
+
+//controller.RegisterCallbackForPinValueChangedEvent(pin, PinEventTypes.Rising, (pin, value) =>
+//{
+//    c.WriteLine($"Rising! Pin {pin} changed to {value}");
+//});
+
+// exit warning
+AppDomain.CurrentDomain.ProcessExit += (sender, args) =>
+{
+    c.WriteLine("Warning: Subsystem is shutting down. Press any key to continue, or 'r' to restart.");
+    var key = c.ReadKey();
+    if (key.KeyChar == 'r') // restart subsystem
+        try { Process.Start(Process.GetCurrentProcess().MainModule!.FileName!); }
+        catch (Exception) { c.WriteLine("Error restarting subsystem."); }
+    //controller.ClosePin(pin);
+};
+
+c.ReadKey();
