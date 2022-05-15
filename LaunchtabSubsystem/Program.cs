d77b0904f0784dc9426c7dff88b82a902b9c681f
Naquino14 <Naquino14@outlook.com>
2022-05-15 14:33:53 -0400

    wip cpu temp reading

--- LaunchtabSubsystem/Program.cs
+++ LaunchtabSubsystem/Program.cs
@@ -7,15 +7,22 @@ using System.Diagnostics;
 using System.Threading;
 
 c.WriteLine("Subsystem is now running. Press ctrl+c to exit.");
-int powerButtonPin = 3;
+
+int powerButtonPin = 3, fanPin = 4;
 bool pressedState = false;
+const int fanOnTemp = 75;
+
 using var controller = new GpioController();
 controller.OpenPin(powerButtonPin, PinMode.Input);
+controller.OpenPin(fanPin, PinMode.Output);
 
 //pin update callbacks
 controller.RegisterCallbackForPinValueChangedEvent(powerButtonPin, PinEventTypes.Falling, (pin, value) => { pressedState = false; });
 
 controller.RegisterCallbackForPinValueChangedEvent(powerButtonPin, PinEventTypes.Rising, (pin, value) => { pressedState = true; });
+
+Action fanOn = () => { controller.Write(fanPin, PinValue.Low); }; // pnp transistor
+Action fanOff = () => { controller.Write(fanPin, PinValue.High); };
 
 // exit warning
 AppDomain.CurrentDomain.ProcessExit += (sender, args) =>
@@ -38,6 +45,10 @@ int gestLoopCounter = 0;
 bool tap = false, doubleTapped = false, held = false, recording = false;
 for (;;)
 {
+    // get cpu temp
+    var temp = int.Parse(new Process(){ StartInfo = new("vcgencmd", "measure_temp") }.Start().StandardOutput.ReadToEnd().Trim());
+    c.WriteLine($"CPU temp: {temp}");
+
     if (pressedState) // todo: fix logic
     {
         tap = !tap;
